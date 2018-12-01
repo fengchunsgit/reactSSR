3309d355708ee7de6b35301bd371c164ecb81565
fengchunsgit <1055688803@qq.com>
2018-12-01 23:34:55 +0800

    webpack 配置

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-var express=require(express)
-
-var app=express();
+const express=require('express')
+const app=express();
+const Home=require('./containers/Home')
 
 app.get('/',function(req,res){
   res.send(`
   <html>
   <h1>hello</h1>
-  </html>
-  `);
+  </html>`
+  );
 });
 
 var server=app.listen(3000);
