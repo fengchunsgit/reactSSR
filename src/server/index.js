b64790e799cd8e4e471d97540b4367a5ac556192
fengchunsgit <1055688803@qq.com>
2018-12-02 21:47:45 +0800

    优化代码结构

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,30 @@
 import express from 'express'
 import {render} from './utils'
+import getStore from '../store'
+import routes from '../Routes'
+import {matchRoutes} from 'react-router-config'
 
 const app=express();
 app.use(express.static('public'))
 
 app.get('*',function(req,res){
-  render(req,res)
+  const store=getStore()
+  const matchedRoutes=matchRoutes(routes,req.path)
+  //这里拿到异步数据，填充到store
+  //store里面填什么，需要结合用户请求地址和路由做判断
+  //如果访问 /
+  //如果访问/login
+  //根据路由的路径，来往store加数据
+  const promises=[]
+  matchedRoutes.forEach(item=>{
+    if(item.route.loadData){
+      promises.push(item.route.loadData(store))
+    }
+  })
+
+  Promise.all(promises).then(()=>{
+    res.send(render(store,routes,req))
+  })
 });
 
 var server=app.listen(3000);
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,27 +1,10 @@
 import {renderToString} from 'react-dom/server'
 import {StaticRouter,Route,matchPath} from 'react-router-dom'
-import routes from '../Routes'
+
 import React from 'react'
 import {Provider} from 'react-redux'
-import getStore from '../store'
-import {matchRoutes} from 'react-router-config'
 
-export const render=(req,res)=>{
-  const store=getStore()
-  const matchedRoutes=matchRoutes(routes,req.path)
-  //这里拿到异步数据，填充到store
-  //store里面填什么，需要结合用户请求地址和路由做判断
-  //如果访问 /
-  //如果访问/login
-  //根据路由的路径，来往store加数据
-  const promises=[]
-  matchedRoutes.forEach(item=>{
-    if(item.route.loadData){
-      promises.push(item.route.loadData(store))
-    }
-  })
-
-  Promise.all(promises).then(()=>{
+export const render=(store,routes,req)=>{
       const content=renderToString((
       <Provider store={store}>
         <StaticRouter location={req.path} context={{}}>
@@ -34,15 +17,14 @@ export const render=(req,res)=>{
       </Provider>
     ))
 
-    res.send(`
+    return `
       <html>
-        <head><title>SS0R</title></head>
+        <head><title>SSR</title></head>
         <body>
         <div id='root'>${content}</div>
           <script src="./index.js"></script>
         </body>
       </html>
-    `)
-  })
+    `
 
 }
