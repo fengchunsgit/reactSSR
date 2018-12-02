a682188dc3ce3c90b8be117f198191c3e9c56b14
fengchunsgit <1055688803@qq.com>
2018-12-02 10:44:32 +0800

    router

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,26 @@
 import express from 'express'
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import Home from '../containers/Home'
+import {StaticRouter} from 'react-router-dom'
+import Routes from '../Routes'
 
 const app=express();
 app.use(express.static('public'))
 
-const content=renderToString(<Home/>)
+
+
 app.get('/',function(req,res){
+  const content=renderToString((
+    <StaticRouter location={req.path} context={{}}>
+      {Routes}
+    </StaticRouter>
+  ))
+
   res.send(
   `<html>
   <head><title>SS0R</title></head>
   <body>
   <div id='root'>${content}</div>
-    
     <script src="./index.js"></script>
   </body>
   </html>
