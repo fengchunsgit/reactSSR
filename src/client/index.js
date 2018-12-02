6dbeeb6965fe5f38abeb7ab74b95914b5ee5a10b
fengchunsgit <1055688803@qq.com>
2018-12-02 14:41:55 +0800

    store复用

--- src/client/index.js
+++ src/client/index.js
@@ -3,18 +3,12 @@ import ReactDom from 'react-dom'
 import Home from '../containers/Home'
 import {BrowserRouter} from 'react-router-dom'
 import Routes from '../Routes'
-import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-
-const reducer=(state={name:'lee'},action)=>{
-  return state
-}
-const store =createStore(reducer,applyMiddleware(thunk))
+import getStore from '../store'
 
 const App=()=>{
   return (
-    <Provider store={store}>
+    <Provider store={getStore()}>
       <BrowserRouter>
         {Routes}
       </BrowserRouter>
